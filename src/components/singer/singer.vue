abf80caf9a81d6ce3ecdcdeef98ef642d481c9fb
IMac <imac@IMacdeiMac.local>
2017-09-25 17:37:10 +0800

    歌手页数据获取

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,8 +1,28 @@
 <template>
-  <div>歌手页面</div>  
+  <div class="singer" ref="singer">
+  </div>  
 </template>
 <script>
+import {getSingerList} from 'api/singer'
+import {ERR_OK} from 'api/config'
 export default {
+  data() {
+    return {
+      singers: []
+    }
+  },
+  methods: {
+    _getSingerList() {
+      getSingerList().then(res => {
+        if (res.code === ERR_OK) {
+          console.log(res.data.list)
+        }
+      })
+    }
+  },
+  mounted() {
+    this._getSingerList()
+  }
 }
 </script>
 <style lang="scss">
